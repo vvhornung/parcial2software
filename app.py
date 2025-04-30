3b54efcc6e3abd83a2902fbf8b66e8912d1e7ba7
vvhornung <vvhornung@eafit.edu.co>
2025-04-30 07:02:35 -0500

    Agrega .gitignore y excluye el entorno virtual

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+aplicacion = Flask(__name__)
+
+@aplicacion.route('/factorial/<int:val>')
+def factorial(val):
+    if val < 0:
+        return "<h2>No existe el factorial de un número negativo.</h2>"
+    else:
+        resultado = 1
+        for i in range(1, val + 1):
+            resultado *= i
+        return f"<h2>El factorial de {val} es: {resultado}</h2>"
+
+if __name__ == '__main__':
+    aplicacion.run(debug=True)
