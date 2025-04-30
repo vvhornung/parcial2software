0c8c9280e79cc3473ec34c12d99563847abc7165
vvhornung <vvhornung@eafit.edu.co>
2025-04-30 07:37:10 -0500

    creacion app para parcial

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ aplicacion = Flask(__name__)
 @aplicacion.route('/factorial/<int:val>')
 def factorial(val):
     if val < 0:
-        return "<h2>No existe el factorial de un número negativo.</h2>"
+        return "<h2>No existe el factorial de un numero negativo.</h2>"
     else:
         resultado = 1
         for i in range(1, val + 1):
